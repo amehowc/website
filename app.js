bed6fe810ada3b6e48bfb0adaf0fdf13e839f2c8
Arthur Cloche <bobby@Duvs-MacBook-Pro.local>
2023-06-25 14:58:44 -0400

    added the collapsible logic

---
+++ app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const workItems = document.getElementsByClassName("work-item");
+  
+    for (let i = 0; i < workItems.length; i++) {
+      console.log(workItems[i]);
+      workItems[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+  });
