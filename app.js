4931809a280a2b5dd623b1f67c9bc563f6d5f2f3
Arthur Cloche <arthurcloche@gmail.com>
2023-07-29 11:47:14 -0400

    fix creative coding grid size

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
     workItem.addEventListener("click", function () {
       this.classList.toggle("active");
       var content = this.nextElementSibling;
-      
+
       if (content.style.display === "block") {
         content.style.display = "none";
       } else {
@@ -19,31 +19,27 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     workItem.addEventListener("mouseover", () => {
       const lineOne = workItem.querySelector(".work-item-line-one");
-      
       const firstWord = workItem.querySelector(".work-item-client");
       const secondWord = workItem.querySelector(".work-item-info");
-      
       const subContainer = workItem.querySelector(".work-item-container");
       const containerRect = workItem.getBoundingClientRect();
       const subContainerRect = subContainer.getBoundingClientRect();
       const firstWordRect = firstWord.getBoundingClientRect();
       const secondWordRect = secondWord.getBoundingClientRect();
-     
       const margins = 10;
-      const lineOneStart = firstWordRect.right - subContainerRect.left + margins;
+      const lineOneStart =
+        firstWordRect.right - subContainerRect.left + margins;
       const lineOneEnd = secondWordRect.left - containerRect.left - margins;
       lineOne.style.left = lineOneStart + "px";
       lineOne.style.width = lineOneEnd - lineOneStart + "px";
       lineOne.style.display = "block";
-    
     });
-   
+
     workItem.addEventListener("mouseout", (event) => {
-      const lineOne = workItem.querySelector(".work-item-line-one")
-      lineOne.style.display = "none"
+      const lineOne = workItem.querySelector(".work-item-line-one");
+      lineOne.style.display = "none";
       // const lineTwo = workItem.querySelector(".work-item-line-two")
       // lineTwo.style.display = "none"
-    
     });
   }
 
